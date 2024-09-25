22d0c96da0bfe671c7a778b1b5c148bca0f00132
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-25 11:51:21 +0200

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,17 @@
+//let js = "amazing";
+//if (js === "amazing") alert("Javascript is Fun!");
+// alert("JavaScript is FUN");
+
+// Type conversion :
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Ayline"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// Type coercion : this happens whenever an
+// operator is dealing with two values that
+// have diffrent types.
