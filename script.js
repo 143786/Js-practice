5b5a2117c394ebbc3382eedf95548b8a37e1b957
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-26 21:57:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -57,27 +57,27 @@
 
 // Boolean Logic
 
-const hasDriversLicense = true; // A
-const hasGoodVision = true; // B
+// const hasDriversLicense = true; // A
+// const hasGoodVision = true; // B
 
-console.log(hasDriversLicense && hasGoodVision); // AND = &&
-console.log(hasDriversLicense || hasGoodVision); // OR  = ||
-console.log(!hasDriversLicense); // Not = !
+// console.log(hasDriversLicense && hasGoodVision); // AND = &&
+// console.log(hasDriversLicense || hasGoodVision); // OR  = ||
+// console.log(!hasDriversLicense); // Not = !
 
-// if (hasDriversLicense && hasGoodVision) {
+// // if (hasDriversLicense && hasGoodVision) {
+// //   console.log("Sarah is able to drive!");
+// // } else {
+// //   console.log("Someone else should drive...");
+// // }
+
+// const isTired = false; // c
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
 //   console.log("Sarah is able to drive!");
 // } else {
 //   console.log("Someone else should drive...");
 // }
-
-const isTired = false; // c
-console.log(hasDriversLicense && hasGoodVision && isTired);
-
-if (hasDriversLicense && hasGoodVision && !isTired) {
-  console.log("Sarah is able to drive!");
-} else {
-  console.log("Someone else should drive...");
-}
 
 // Challenge 3 :
 
@@ -94,20 +94,90 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 // }
 
 // BONUS :
-const scoreDolphins = (97 + 112 + 81) / 3;
-const scoreKoalas = (109 + 95 + 86) / 3;
-console.log(scoreDolphins, scoreKoalas);
 
-if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
-  console.log("Dolphins win the trophy 🏆🏆");
-} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
-  console.log("Koalas win the trophy 🏆🏆🏆");
-} else if (
-  scoreDolphins === scoreKoalas &&
-  scoreDolphins >= 100 &&
-  scoreKoalas >= 100
-) {
-  console.log("Both win the  🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆");
+// const scoreDolphins = (97 + 112 + 81) / 3;
+// const scoreKoalas = (109 + 95 + 86) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+//   console.log("Dolphins win the trophy 🏆🏆");
+// } else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+//   console.log("Koalas win the trophy 🏆🏆🏆");
+// } else if (
+//   scoreDolphins === scoreKoalas &&
+//   scoreDolphins >= 100 &&
+//   scoreKoalas >= 100
+// ) {
+//   console.log("Both win the  🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆");
+// } else {
+//   console.log("No one win the 🏆🏆🏆");
+// }
+
+// The SWITCH statement :
+
+// const day = "ttt";
+
+// switch (day) {
+//   case "monday": // day === 'monday'
+//     console.log("Plan course structure");
+//     console.log("Go to gym");
+//     break; // without the break the code simply continues
+//   // executing for all case so we need the break at the end
+//   // of every case
+
+//   case "tuesday":
+//     console.log("GO to Bib");
+//     break;
+
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Meeting with ALim and pratice code");
+//     break;
+
+//   case "friday":
+//     console.log("Record videos");
+//     break;
+
+//   case "saturday":
+//   case "sunday":
+//     console.log("Bieng with Ayline and Nil for enjoying our weekend");
+//     break;
+
+//   default:
+//     console.log("Not a valid day!");
+// }
+
+// if (day === "monday") {
+//   console.log("Plan course structure");
+//   console.log("Go to gym");
+// } else if (day === "tuesday") {
+//   console.log("GO to Bib");
+// } else if (day === "wednesday" && day === "thursday") {
+//   console.log("Meeting with ALim and pratice code");
+// } else if (day === "friday") {
+//   console.log("Record videos");
+// } else if (day === "saturday" && day === "sunday") {
+//   onsole.log("Bieng with Ayline and Nil for enjoying our weekend");
+// } else {
+//   console.log("Not a valid day!");
+// }
+
+// The Conditional Operator (Ternary) :
+
+const age = 22;
+const drink = age >= 18 ? "wine🍷" : "water💧";
+console.log(drink);
+
+// we can do the conditional operator in ( if else statement) like this:
+let dring2;
+if (age >= 18) {
+  drink2 = "wine 🍷";
 } else {
-  console.log("No one win the 🏆🏆🏆");
+  dring2 = "water💧";
 }
+
+console.log(drink);
+
+console.log(` I like to drink ${age >= 18 ? "wine🍷" : "water💧"}`);
+
+//
