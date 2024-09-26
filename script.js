f2994367bffcd6035a396d42e0e67898a03b5d5e
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-26 11:39:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,125 +1,113 @@
-// // // let js = "amazing";
-// // // console.log("Tabish");
-
-// // // let firstName = "Tab";
-
-// // // console.log(firstName);
-// // // console.log(firstName);
-// // // console.log(firstName);
-// // // console.log(firstName);
-// // // console.log(firstName);
-
-// // // let myFirstJob = "Coder";
-// // // let myCurrentJob = "Teacher";
-
-// // // console.log(myFirstJob);
-
-// // // let country = "Turkye";
-// // // let continent = "Asia";
-// // // let population = "65millions";
-
-// // // console.log(country);
-// // // console.log(continent);
-// // // console.log(population);
-
-// // const now = 2037;
-// // const tabAge = now - 1991;
-// // const nilAge = now - 2018;
-// // console.log(tabAge, nilAge);
-
-// // console.log(tabAge * 2, tabAge / 2, 2 ** 3);
-
-// // const firstName = 'Ayline';
-// // const lastName = 'Tabish';
-// // console.log(firstName + ' ' + lastName);
-
-// // // Assignment operators
-// // let x = 10 + 5; // 15
-// // x+= 10;   // 25
-// // x*= 4;    // 100
-// // x++;      // 101
-// // x--;
-// // x--;      // 99
-// // console.log(x);
-
-// // // Comparison operators // <, >,  <=,  >=
-
-// // console.log(tabAge > nilAge);
-// // console.log(nilAge >= 18);
-
-// // const isFullAge = nilAge >= 18;
-
-// // // console.log(now - 1991 > now - 2018);
-
-// // const now = 2037;
-// // const ageTab = now - 1991;
-// // const ageNil = now - 2018;
-
-// // console.log(now - 1991 > now - 2018);
-
-// // let x, y ;
-// // x = y = 25-10-5;
-// // console.log(x,y);
-
-// // const averageAge = (ageNil+ageTab) / 2
-// // console.log(ageNil, ageTab, averageAge);
-
-// const lastName = 'tabish';
-// const job = 'developper';
-// const birthYear = 1991;
-// const year = 2037;
-
-// const tabish = "i'm " + lastName  + ', a ' + (year - birthYear) + ' year old ' + job + '!';
-// console.log(tabish)
-
-// const tabishNew = `I'm ${lastName}, a ${year - birthYear} year old ${job}!`; // template literrale
-// console.log(tabishNew);
-
-// console.log('String with \n\
-// multiple \n\
-// lines');
-
-// const age = 15;
-// if (age >= 18) {
-//   console.log("Sarah can start driving licence 🚗");
-// } else {
-//   const yearsLeft = 18 - age;
-//   console.log(`Sara is too young. wait another ${yearsLeft} years :)`);
-// }
-
-// const birthYear = 2012;
-
-// let century;
-// if(birthYear <=2000) {
-//   century = 20;
-// } else {
-//   century = 21;
-// }
-// console.log(century);
-
-// const inputYear = 1991;
-// console.log(inputYear);
-
 // type conversion
 
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
 
-console.log(Number("Ayline"));
-console.log(typeof Nan);
+// console.log(Number("Ayline"));
+// console.log(typeof Nan);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
 // Type coercion
-console.log(`I am ` + 23 + ` years old `);
-console.log("23" - "10" - 3);
-console.log("23" / "2");
-
-let n = "1" + 1; // '11' here the number converted to string because of plus (+)
-n = n - 1; // here the string converted to number becuse of minus (-)
-console.log(n);
+// console.log("I am" + 23 + "years old");
+// console.log("23" - "10" - 3);
+// console.log("23" / "2");
 
 // 5 falsy values : 0, '', undefined, null, NaN,
 
-console.log(Boolean(0));
+// console.log(Boolean(0)); // false
+// console.log(Boolean(undefined)); //
+// console.log(Boolean("Ayline")); //
+// console.log(Boolean("")); // it's a empty string
+// console.log(Boolean({})); // it's a empty object
+
+// const money = 2300;
+// if (money) {
+//   console.log("Don't spend it all ;");
+// } else {
+//   console.log("you should get a job!");
+// }
+
+// // Equality operators :
+
+// const age = "18";
+// if (age === 18) console.log("You just become an adult :D (strict)");
+
+// whenever or if block only has one line we don't need
+// to curly braces {}.
+
+// if (age == 18) console.log("You just become an adult :D (loose)");
+
+// const fav = Number(prompt("Whats your fav number ?"));
+// console.log(fav);
+// console.log(typeof fav);
+// if (fav === 10) {
+//   console.log("Cool! 10 is an an amazing number");
+// } else if (fav === 7) {
+//   console.log("7 is also a cool number");
+// } else if (fav === 8) {
+//   console.log("8 is the number of Kroos in Real Madrid");
+// } else if (fav === 9) {
+//   console.log("9 is the number of Mbapé in Real Madrid");
+// } else {
+//   console.log("Number is not 10 , 7 , 8 or 9");
+// }
+// if (fav !== 10) console.log("why not 10?");
+
+// Boolean Logic
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision); // AND = &&
+console.log(hasDriversLicense || hasGoodVision); // OR  = ||
+console.log(!hasDriversLicense); // Not = !
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+const isTired = false; // c
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive...");
+}
+
+// Challenge 3 :
+
+// const scoreDolphins = (96 + 108 + 110) / 3;
+// const scoreKoalas = (96 + 108 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy 🏆");
+// } else if (scoreDolphins < scoreKoalas) {
+//   console.log("Koalas win the trophy 🏆");
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log("Both win the  🏆");
+// }
+
+// BONUS :
+const scoreDolphins = (97 + 112 + 81) / 3;
+const scoreKoalas = (109 + 95 + 86) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("Dolphins win the trophy 🏆🏆");
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+  console.log("Koalas win the trophy 🏆🏆🏆");
+} else if (
+  scoreDolphins === scoreKoalas &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
+  console.log("Both win the  🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆");
+} else {
+  console.log("No one win the 🏆🏆🏆");
+}
