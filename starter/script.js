4f2873cd3fd28a3058f03ea1ca3ea0e7d665e1cc
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-27 15:53:36 +0200

    Add files via upload

--- starter/script.js
+++ starter/script.js
@@ -1,17 +1,55 @@
-//let js = "amazing";
-//if (js === "amazing") alert("Javascript is Fun!");
-// alert("JavaScript is FUN");
+"use strict";
+// function tab() {
+//   console.log("Ayline");
+// }
 
-// Type conversion :
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// // calling / runnning / invoking the functions
 
-console.log(Number("Ayline"));
-console.log(typeof NaN);
+// tab();
+// tab();
+// tab();
 
-console.log(String(23), 23);
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
 
-// Type coercion : this happens whenever an
-// operator is dealing with two values that
-// have diffrent types.
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangesJuice = fruitProcessor(2, 4);
+// console.log(appleOrangesJuice);
+
+// Function Declaration
+
+// function great(name) {
+//   return `Hello, ${name}!`;
+// }
+
+// console.log(great("Ayline"));
+
+// great();
+
+// function great() {
+//   console.log("Hello!");
+// }
+
+// Function Declaration :
+function calcAge1(birthYear) {
+  return 2024 - birthYear;
+}
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// Function expression :
+const calcAge2 = function (birthYear) {
+  return 2024 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age2);
+
+// Arrow Function:
+const calcAge3 = (birthYear) => 2024 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
