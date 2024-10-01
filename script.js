8a010e530cacfe666c56dc10e40cde2a7c0b2030
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-10-01 16:11:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,122 @@
 "use strict";
-function tab() {
-  console.log("Ayline");
-}
+// function tab() {
+//   console.log(" I love Ayline! ");
+// }
 
-// calling / runnning / invoking the functions
+// // calling / runnning / invoking the functions
 
-tab();
-tab();
-tab();
+// tab();
+// tab();
+// tab();
 
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `juice with ${apples} apples and ${oranges} oranges`;
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangesJuice = fruitProcessor(2, 4);
+// console.log(appleOrangesJuice);
+
+// Function Declaration
+
+// function great(name) {
+//   return `Hello, ${name}!`;
+// }
+
+// console.log(great("Ayline"));
+
+// great();
+
+// function great() {
+//   console.log("Hello!");
+// }
+
+// Function Declaration :
+// function calcAge1(birthYear) {
+//   return 2024 - birthYear;
+// }
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+// // Function expression :
+// const calcAge2 = function (birthYear) {
+//   return 2024 - birthYear;
+// };
+// const age2 = calcAge2(1991);
+// console.log(age2);
+
+// // Arrow Function:
+// const calcAge3 = (birthYear) => 2024 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear) => {
+//   const age = 2024 - birthYear;
+//   const retirement = 65 - age;
+//   return retirement;
+// };
+
+// console.log(yearsUntilRetirement(1991));
+
+// // Arow functions with multi parameters :
+
+// // Functions Calling Other functions:
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years `);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired 🎉 `);
+//     return -1;
+//   }
+// };
+
+// console.log(yearsUntilRetirement(2023, "AYline"));
+// console.log(yearsUntilRetirement(1950, "Nil"));
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(23, 34);
+// console.log(appleOrangeJuice);
+
+// const appleJuice = fruitProcessor(5, 0);
+
+function fruitProcessor(apples, oranges, kiwis) {
+  const juice = `juice with ${apples} apples , ${oranges} oranges and ${kiwis} kiwis.`;
   return juice;
 }
 
-const appleJuice = fruitProcessor(5, 0);
+const appleJuice = fruitProcessor(4, 0, 0);
 console.log(appleJuice);
+
+const appleKiwiJuice = fruitProcessor(5, 0, 9);
+console.log(appleKiwiJuice);
+
+const kiwiOrangeJuice = fruitProcessor(0, 23, 44);
+console.log(kiwiOrangeJuice);
+
+const appleOrangeKiwiJuice = fruitProcessor(12, 23, 44);
+console.log(appleOrangeKiwiJuice);
