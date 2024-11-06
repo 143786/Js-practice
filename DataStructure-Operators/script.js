e847593b716e4fb4136b916e035151a2dadffb9d
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-11-06 14:52:40 +0100

    Add files via upload

--- DataStructure-Operators/script.js
+++ DataStructure-Operators/script.js
@@ -4,9 +4,16 @@ const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  startMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  // we add a method (function to order food)
 };
+
+// -- ** Destructuring Arrays ** ---
 
 const arr = [2, 3, 4];
 const a = arr[0];
@@ -16,3 +23,26 @@ const c = arr[2];
 const [x, y, z] = arr;
 
 console.log(x, y, z);
+
+let [main, , secondary] = restaurant.categories;
+
+console.log(main, secondary);
+
+// ** - Switching varibales **
+
+/* const temp = main;
+main = secondary;
+secondary = temp;
+console.log(main, secondary); */
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// ** -- Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
