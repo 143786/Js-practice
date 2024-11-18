8d901a35f9ee32f851f3d28f3e1862dd98e0c567
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-11-18 14:45:36 +0100

    Add files via upload

---
+++ LOOP/script.js
@@ -0,0 +1,27 @@
+"use strict";
+/* for LOOP :  the for loop has 3 step :
+1) Initial value,
+2) When to run the loop (condtions),
+3) How to change  value each time normally (+1);  */
+
+// for (let i = 0; i <= 20; i++) {
+//   console.log(i);
+// }
+
+// for (let num = 50; num >= 0; num -= 10) {
+//   console.log(num);
+// }
+
+// for (let i = 100; i >= 0; i -= 12) {
+//   console.log(i);
+// }
+
+// for (let i = 200; i >= 0; i -= 25) {
+//   console.log(i);
+// }
+
+// DON'T RUN THIS ! - infinite loop :
+
+// for (let i = 1; i !== 20; i += 2){
+//   console.log(i);
+// }
