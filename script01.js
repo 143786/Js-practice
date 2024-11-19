43343652ec4c69afc881ed646a092954269baf71
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-11-19 23:59:16 +0100

    Add files via upload

--- script01.js
+++ script01.js
@@ -1,1 +1,10 @@
-// *** Rest Patern : look exactly like the spread operator
+/* *** Rest Patern : look exactly like the spread operator. 
+it has the same syntax with the ...  but it actually does the opposite 
+of the spread operator.
+*** we use the spread operator to build new arrays or to passe multiple
+values into a function. so those are the two use case of the spread operator.
+and in the both cases, we use the spread operator to expand an array into
+individual elements. ***  the rest pattern uses the exact same syntax. however,
+to collect multiple elements and condense them into an array. so that's really
+the opposite of spread; the spread operator is to unpack an array while rest
+is to pack elements into an array. */
