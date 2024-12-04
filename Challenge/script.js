5baa46a6a978e941ad6e3f0a75cbfb4e2abbaf9a
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-12-04 15:07:10 +0100

    Add files via upload

--- Challenge/script.js
+++ Challenge/script.js
@@ -13,7 +13,7 @@ const game = {
       "Goretzka",
       "Coman",
       "Muller",
-      "Gnarby",
+      "Gnabry",
       "Lewandowski",
     ],
     [
@@ -31,7 +31,7 @@ const game = {
     ],
   ],
   score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  scored: ["Lewandowski", "Gnabry", "Lewandowski", "Hummels"],
   date: "Nov 9th, 2037",
   odds: {
     team1: 1.33,
@@ -39,3 +39,47 @@ const game = {
     team2: 6.5,
   },
 };
+
+//  The solution of this challenge here in 7 step ..... :
+
+// 1.
+
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2.
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3. Here we use the SPREAD OPERATOR...
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4.
+
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+console.log(players1Final);
+
+// 5. Nested destructuring here :
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6.
+
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+
+// printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+// printGoals("Davies", "Muller");
+
+printGoals(...game.scored);
+
+// 7.
+
+team1 < team2 && console.log("Team 1 is more likely to win");
+team1 > team2 && console.log("Team 2 is more likely to win");
