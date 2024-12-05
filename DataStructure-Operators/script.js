3fac5760c3ba2e787f949dd314d394150a578a1f
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-12-05 11:29:53 +0100

    Add files via upload

--- DataStructure-Operators/script.js
+++ DataStructure-Operators/script.js
@@ -1,5 +1,34 @@
 'use strict';
 
+/*  ***** Enhanced Object Literals :  
+here this restaurant object is an object literal, because we wrote this object literally 
+in our code using the curly braces syntax, so all of this  object here has been written using
+the object literal syntax. Now ES6 introduced 3 ways, which make it easier to write object literal
+like this so let's go through them one by one now.
+First we can say that we have an object that is outside of this object. we take the ( openingHours ) 
+object and create a separate object with it.
+
+
+THe second enhancement to object literals is about writing  methods. 
+so in ES6 we no longer have to create a property, and then set it to a functon expression, like 
+we have always been doing, 
+*/
+const weekdays = ['mon', 'tue', 'wed', 'the', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // open 24 hours
+    close: 24,
+  },
+};
+
 /*  ** --- Destructuring Object --- for doing that we use the 
 curly braces {} because this is also how we crate objects. then 
 then all we have to do is to provide the variable names that exactly match the 
@@ -11,56 +40,145 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // open 24 hours
-      close: 24,
-    },
-  },
+  // ES6 enchanced object literals
+  openingHours,
 
   // we add a method (function to order food)
-  order: function (starterIndex, mainIndex) {
+
+  /*  
+  THe second enhancement to object literals is about writing  methods. 
+so in ES6 we no longer have to create a property, and then set it to a functon expression, like 
+we have always been doing, but now we no longer need to do that  we can write it in an easier way
+ here we remove ( : function ) no needed in ES6 : we remove for all functions :
+  it's the new way of writing mehtods, but this actually comes down to personal preference
+  once again. so if you prefer the old way you can just keep ot likt that.
+  so maybe you think it's more explicit if you can actually read the function keyword there,
+  but for me it's enouph that we have the parenthesis here , so that alone already shows that 
+  if has to be a function and VS code itself is also helpful as it change the color of the methods
+  to green, so VS code is really smart in that regard.
+
+  */
+  // order: function (starterIndex, mainIndex) {
+  // return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
+
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
-    console.log(
-      `order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
-      will be delivered to ${address} at ${time} `
-    );
-  },
-  orderPasta: function (ing1, ing2, ing3) {
-    // here we add another method (oderPasta)
-    console.log(
-      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
-    );
-  },
+  // orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+  //   console.log(
+  //     `order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}
+  //     will be delivered to ${address} at ${time} `
+  //   );
+  // },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  // orderDelivery({ starterIndex, mainIndex, time, address }) {
+  // //   //console.log(
+  // //     `order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}
+  // //     will be delivered to ${address} at ${time} `
+  // //   );
+  // // },
+  // // orderPasta: function (ing1, ing2, ing3) {
+  // //   // here we add another method (oderPasta)
+  // //   console.log(
+  // //     `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+  // //   );
+  // // },
+
+  // orderPasta(ing1, ing2, ing3) {
+  //   // here we add another method (oderPasta)
+  //   console.log(
+  //     `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+  //   );
+  // },
+
+  // orderPizza: function (mainIngredient, ...otherIngredients) {
+  //   console.log(mainIngredient);
+  //   console.log(otherIngredients);
+  // },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
 
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+/*  -- ***** Optional chaining => THis feature interduced in ES2020 *** 
+if a certain property does not exist, then undefined is returned immediately.  and so that will
+then avoid that kind of error that we sa earlier :  istead of dot (.) we use question mark, dot 
+then open, so here is how optional chaining works :
+
+ */
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+/*  so only if the property that is before this question mark here, so only if Monday exist,
+Then this open property will be read from there.  But if not, the immediately undefined will be
+returned. and exist here actullay means the nullish concept, so if it's zero or the empty String, 
+  then it still exists, soif we try this now, then we get undefined.*/
+
+//  Example of Optional chaining :
+
+const days = ['mon', 'tue', 'wed', 'the', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'cloded';
+  // here we use the optional chaining operator (?.) and the nullish coalescing operator (??)
+  // working together. they were introduced at the same time in ES2020. they were design to work
+  // with each other. so both of them rely on this new concept of nullish so nullish values are
+  // null and undefined.
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+/*  ****    --- Looping Arrays : The for-of Loop -- introduced in ES6  : 
+This loop will automatically loop over the entire array and in each iteration, it 
+will give us access to the current array element which we can specify here. so 
+in this case it's called the item, but of course we could call it anything that we want.
+And if we simply log the current item in to console we get  all elements, simply each 
+element logged one by one. and that's because again, the item variable is always the current
+element in each iteration. and also just like in the if else statement, we don't need to create
+a code block when we only have one statement here to execute, so that's pretty simple,  but it's 
+really nice level of obstructing over the regular for loop. so we can do the same thing with this
+one,  but without having to worry about all the underlying details such as counters and conditions.
+
+what's also great about the for-of loop, is that we can still use the continue and break keywords.
+
+ */
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+
+/* But now what if we also wanted the current index and not just the current element, il the for-of loop
+it's a bit of a pain when we need that index, because originally the for-of loop was really just 
+meant to give you the current element. However, we can get both and we can do it like this :::
+*/
+
+// for (const [item] of menu.entries()) {
+//   console.log(`${item[0] + 1}: ${item[1]}`);
+// }
+
+// wen want to destructuring the item here for made it easy  :
+// destructuring made our lives a litte bit easier here.
+
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
+
 // ****   Logical Assignment operators : introduced in ES 2021 :
 
-const rest1 = {
-  name: 'Capri',
-  numGuests: 0,
-};
+// const rest1 = {
+//   name: 'Capri',
+//   numGuests: 0,
+// };
 
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi',
-};
+// const rest2 = {
+//   name: 'La Piazza',
+//   owner: 'Giovanni Rossi',
+// };
 
 /* ****  OR ||=  assignment operator :
  This operator assigns a variable to a variable if that variable is currently falsy, like this
@@ -79,26 +197,26 @@ const rest2 = {
 the nuliish assignement operator will assign a value to a variable if that exact varibale is 
 currently nullish so again, that is the case in restaurant number2
 */
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
 
-// AND assignment operator :
-// rest2.owner = rest2.owner && '<ANONYMOUS>';
-// rest1.owner = rest1.owner && '<ANONYMOUS>';
-// We can write it like this in shortcut way :
-rest2.owner &&= '<ANONYMOUS>';
-rest1.owner &&= '<ANONYMOUS>';
+// // AND assignment operator :
+// // rest2.owner = rest2.owner && '<ANONYMOUS>';
+// // rest1.owner = rest1.owner && '<ANONYMOUS>';
+// // We can write it like this in shortcut way :
+// rest2.owner &&= '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
 
-console.log(rest1);
-console.log(rest2);
+// console.log(rest1);
+// console.log(rest2);
 
 // restaurant.numGuests = 0;
 // const guests = restaurant.numGuests || 10;
 // console.log(guests);
-// /*  *** Nullish : null and undefinded (NOT 0 or '' )
-//  nullish coalescing operator : it' an operator that was introduced in ES2020,
-// and it works like this :
-//  */
+/* ///////////////////////////// *** Nullish : null and undefinded (NOT 0 or '' )  ??
+ nullish coalescing operator : it' an operator that was introduced in ES2020,
+ and it works like this :
+*/
 // const guestsCorrect = restaurant.numGuests ?? 10;
 // console.log(guestsCorrect);
 
