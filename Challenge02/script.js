7f0aa5bdbc22f9198e7aca70e6f955be0cda5588
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-12-06 14:50:45 +0100

    Add files via upload

---
+++ Challenge02/script.js
@@ -0,0 +1,53 @@
+"use strict";
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnabry",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnabry", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//  The solution of this challenge here in 4 step ..... :
+
+// 1.
+for (const [i, player] of game.scored.entries())
+  console.log(`Goal ${i + 1} : ${player}`);
+
+// 2.
+
+// 3. Here we use the SPREAD OPERATOR...
+
+// 4.
